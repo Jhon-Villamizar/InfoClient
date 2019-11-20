ccccc94447efd369dc776bcfa7be548276389e10
Jhon Villamizar <jhonvio@hotmail.com>
2019-11-19 19:18:52 -0500

    inicio login

--- View-InfoClient/src/app/components/nav/nav.component.ts
+++ View-InfoClient/src/app/components/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SellerService } from 'src/app/service/seller.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sellerService: SellerService) { }
 
   ngOnInit() {
+    this.getSellers();    
+  }
+
+  getSellers(){
+    this.sellerService.getSellers()
+      .subscribe(res =>{
+        this.sellerService.sellers = res as {};
+        console.log('seller',res);
+      })
   }
 
 }
